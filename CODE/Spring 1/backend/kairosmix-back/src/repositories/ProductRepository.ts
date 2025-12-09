fbddf5f5fe77dc6cddb7e3178273c2d238cfb2da
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-12-08 20:02:58 -0500

    ADD findById method to ProductRepository

--- CODE/Spring 1/backend/kairosmix-back/src/repositories/ProductRepository.ts
+++ CODE/Spring 1/backend/kairosmix-back/src/repositories/ProductRepository.ts
@@ -3,7 +3,8 @@ import Product, { IProduct } from '../models/Product.js';
 export interface IProductRepository {
     create(data: Partial<IProduct>): Promise<IProduct>;
     countByInitial(initial: string): Promise<number>;
-    findAll(): Promise<IProduct[]>; // <--- AGREGAR ESTO
+    findAll(): Promise<IProduct[]>;
+    findById(id: string): Promise<IProduct | null>;
 }
 
 export class MongoProductRepository implements IProductRepository {
@@ -17,9 +18,16 @@ export class MongoProductRepository implements IProductRepository {
         });
     }
 
-    // <--- IMPLEMENTAR ESTO
     async findAll(): Promise<IProduct[]> {
         // Solo devolvemos los activos (Eliminación lógica)
         return await Product.find({ isActive: true });
+    }
+
+    async findById(id: string): Promise<IProduct | null> {
+        const product = await Product.findById(id);
+        if (!product || !product.isActive) {
+            return null;
+        }
+        return product;
     }
 }
