525af56af2347d5ef0d158606809b180e54a4941
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-12-09 17:12:57 -0500

    ADD get Product By Id

--- CODE/Spring 1/frontend/kairosmix-front/src/services/productService.js
+++ CODE/Spring 1/frontend/kairosmix-front/src/services/productService.js
@@ -40,3 +40,26 @@ export const updateProduct = async (id, productData) => {
            throw error;
        }
    };
+
+
+export const searchProducts = async (query) => {
+       try {
+           const response = await fetch(`${API_URL}/search?q=${encodeURIComponent(query)}`);
+           if (!response.ok) throw new Error('Error en la búsqueda');
+           return await response.json();
+       } catch (error) {
+           console.error('Error:', error);
+           throw error;
+       }
+   };
+
+      export const getProductById = async (id) => {
+       try {
+           const response = await fetch(`${API_URL}/${id}`);
+           if (!response.ok) throw new Error('Producto no encontrado');
+           return await response.json();
+       } catch (error) {
+           console.error('Error:', error);
+           throw error;
+       }
+   };
